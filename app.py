f71ab09d09871014c73caa8b83d6adf47e1b7581
mybuildwithai-hub <mybuildwithai@gmail.com>
2026-01-04 13:11:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,55 @@
 import streamlit as st
+import time
 
-st.set_page_config(page_title="Toddler Sounds", page_icon="🔊")
+st.set_page_config(page_title="Toddler Fun", page_icon="🐾")
 
-st.title("Touch the Animals! 🔊")
+# 1. Initialize "Memory": If this is the first time opening the app, 
+# tell Python we aren't showing a big animal yet.
+if 'showing_animal' not in st.session_state:
+    st.session_state.showing_animal = None
 
-# Create three columns for more choices
-col1, col2, col3 = st.columns(3)
+# 2. SCREEN A: Show the Big Animal
+if st.session_state.showing_animal:
+    animal = st.session_state.showing_animal
+    
+    st.title(f"LOOK! A {animal['name'].upper()}!")
+    
+    # Show the big image
+    st.image(animal['image_url'], use_container_width=True)
+    
+    # Play the sound
+    st.audio(animal['sound_url'], autoplay=True)
+    
+    # Wait for 5 seconds
+    time.sleep(5)
+    
+    # Clear the memory and refresh to go back to the main menu
+    st.session_state.showing_animal = None
+    st.rerun()
 
-with col1:
-    if st.button('🦁 Lion', use_container_width=True):
-        st.write("ROAR!")
-        # A link to a short lion roar sound
-        st.audio("https://www.w3schools.com/html/lion.ogg") # Placeholder: Replace with Lion URL
-        st.balloons()
+# 3. SCREEN B: The Main Menu (The grid of animals)
+else:
+    st.title("Tap an animal! 🐾")
+    
+    col1, col2 = st.columns(2)
 
-with col2:
-    if st.button('🐶 Dog', use_container_width=True):
-        st.write("WOOF!")
-        st.audio("https://www.soundjay.com/nature/dog-bark-1.mp3")
-        st.snow()
+    with col1:
+        # We store the animal data in a "Dictionary" (a Python list of labels)
+        lion_data = {
+            "name": "Lion",
+            "image_url": "https://img.freepik.com/free-vector/cute-lion-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-flat-vector_138676-1335.jpg",
+            "sound_url": "https://www.w3schools.com/html/horse.ogg" # Swap with real Lion MP3 later!
+        }
+        if st.button('🦁 Tap the Lion', use_container_width=True):
+            st.session_state.showing_animal = lion_data
+            st.rerun()
 
-with col3:
-    if st.button('🐱 Cat', use_container_width=True):
-        st.write("MEOW!")
-        st.audio("https://www.soundjay.com/condition/sounds/cat-meow-1.mp3")
-        st.toast("Meow!") # This pops up a tiny notification at the bottom
+    with col2:
+        dog_data = {
+            "name": "Dog",
+            "image_url": "https://img.freepik.com/free-vector/cute-dog-sitting-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-flat-vector_138676-1336.jpg",
+            "sound_url": "https://www.soundjay.com/nature/dog-bark-1.mp3"
+        }
+        if st.button('🐶 Tap the Dog', use_container_width=True):
+            st.session_state.showing_animal = dog_data
+            st.rerun()
